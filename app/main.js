51a6be7535d0807da1972e7fe950265615f94b3a
mnokeefe <matt@mattokeefe.co.uk>
2015-11-06 21:56:37 +0000

    receiving midi messages

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 /*  --------------------------------------------------- 
-    Our app entrypoint which crawls the DOM for components
+		Our app entrypoint which crawls the DOM for components
 ------------------------------------------------------- */
 
 // import './common';
@@ -18,23 +18,37 @@
 // 	this.component = new Component($element);
 // });
 
-import 'web-midi-api/WebMIDIAPI.min.js';
-
-
-class Main {
-
-	constructor() {
-		navigator.requestMIDIAccess().then(this.onFulFilled, this.onRejected);
-	}
-
-	onFulFilled() {
-		console.log('supported');
-	}
-
-	onRejected() {
-		console.log('rejected');
-	}
+var midi, data;
+// request MIDI access
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess({
+        sysex: false
+    }).then(onMIDISuccess, onMIDIFailure);
+} else {
+    alert("No MIDI support in your browser.");
 }
 
+// midi functions
+function onMIDISuccess(midiAccess) {
+    // when we get a succesful response, run this code
+    midi = midiAccess; // this is our raw MIDI data, inputs, outputs, and sysex status
+
+    var inputs = midi.inputs.values();
+    // loop over all available inputs and listen for any MIDI input
+    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+        // each time there is a midi message call the onMIDIMessage function
+        input.value.onmidimessage = onMIDIMessage;
+    }
+}
+
+function onMIDIFailure(error) {
+    // when we get a failed response, run this code
+    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + error);
+}
+
+function onMIDIMessage(message) {
+    data = message.data; // this gives us our [command/channel, note, velocity] data.
+    console.log('MIDI data', data); // MIDI data [144, 63, 73]
+}
 
 
